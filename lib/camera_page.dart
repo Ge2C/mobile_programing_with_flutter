b9301d08043eeb20182842ec3483f1d300a2f7af
Ge2C <gw.boy0@gmail.com>
2024-10-21 11:20:13 +0700

    Membuat halaman kamera

---
+++ lib/camera_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Camera"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(50, 50, 50, 30),
+              child: Text("Camera page")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
