dad58ea21a0dec5f8e4b40d936574db9f0514b48
Ge2C <gw.boy0@gmail.com>
2024-10-21 13:44:02 +0700

    Update rest_api_page.dart

--- lib/rest_api_page.dart
+++ lib/rest_api_page.dart
@@ -1,4 +1,6 @@
+import 'dart:convert'; // Untuk mengubah data JSON menjadi Map
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class RestApiPage extends StatefulWidget {
   const RestApiPage({super.key});
@@ -8,6 +10,27 @@ class RestApiPage extends StatefulWidget {
 }
 
 class _RestApiPageState extends State<RestApiPage> {
+  List<dynamic> _data = [];
+
+  Future<void> _fetchData() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        _data = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +42,31 @@ class _RestApiPageState extends State<RestApiPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50, 50, 50, 30),
-              child: Text("REST API Page"),
+            Expanded(
+              child: _data.isEmpty
+                  ? Center(child: CircularProgressIndicator())
+                  : ListView.builder(
+                      itemCount: _data.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(_data[index]['title']),
+                          subtitle: Flexible(
+                            child: RichText(
+                              overflow: TextOverflow.ellipsis,
+                              strutStyle: StrutStyle(
+                                fontSize: 12.0,
+                              ),
+                              text: TextSpan(
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                                text: _data[index]['body'],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
             ),
           ],
         ),
