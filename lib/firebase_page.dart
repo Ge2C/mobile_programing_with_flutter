cce177dbbd015834a87fb67b3a1c832ce6d2c55c
Ge2C <gw.boy0@gmail.com>
2024-10-27 21:44:26 +0700

    Integrasikan clode firestore ke dalam halaman firebase_page

--- lib/firebase_page.dart
+++ lib/firebase_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebasePage extends StatefulWidget {
   const FirebasePage({super.key});
@@ -8,6 +9,9 @@ class FirebasePage extends StatefulWidget {
 }
 
 class _FirebasePageState extends State<FirebasePage> {
+  var db = FirebaseFirestore.instance;
+  List<Map<String, dynamic>> users = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
